53b194f777d9fa052e40727d5f57db1993db03d2
coding-manuel <gautam.manuel@gmail.com>
2022-11-11 10:32:15 +0530

    Added Style Yourself Button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import { Button } from "@mantine/core";
 import React from "react";
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <Layout>
-      <div></div>
+      <div>
+        <Button component={Link} to="/creator">
+          Style Yourself
+        </Button>
+      </div>
     </Layout>
   );
 }
