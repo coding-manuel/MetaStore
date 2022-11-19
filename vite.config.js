703d1406e54c629b56961f721dcacc5cfd26dcac
coding-manuel <gautam.manuel@gmail.com>
2022-11-19 20:45:23 +0530

    config update

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
-import path from 'path'
-import inject from '@rollup/plugin-inject'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-        '@': path.resolve(__dirname, 'src'),
-    }
-},
-build: {
-    rollupOptions: {
-        plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
-    },
-},
 })
