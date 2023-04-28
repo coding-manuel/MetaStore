4fb667a15914ff5c1c247df8eca3fafdc31b86fb
Gautam Manuel <gautam.manuel@gmail.com>
2023-04-28 20:05:48 +0530

    new css

--- src/pages/ARView.jsx
+++ src/pages/ARView.jsx
@@ -3,60 +3,44 @@ import { ARButton, XR, useHitTest } from "@react-three/xr"
 import { Canvas } from "@react-three/fiber"
 import { Environment, Html } from "@react-three/drei"
 import Character from "../components/Creator/Character"
-import { Box, Button, Center, Group, Loader, Stack } from "@mantine/core"
+import {
+  Container,
+  Box,
+  Button,
+  Center,
+  Group,
+  Loader,
+  Stack,
+} from "@mantine/core"
+import "./ar.css"
 
 function Experience() {
   const [select, setSelect] = useState(false)
   return (
-    // <div>
-    //   <ARButton />
-    //   <Canvas dpr={[1, 2]} shadows camera={{ position: [0, 2, 0] }}>
-    //     <XR referenceSpace="local-floor">
-    //       {select ? <Character /> : <Marker />}
-    //       <ambientLight intensity={0.2} />
-    //       <Html>
-    //         <button onClick={() => setSelect(!select)}>chicken</button>
-    //       </Html>
-    //       {/* <Character /> */}
-    //       <Environment preset="city" />
-    //     </XR>
-    //   </Canvas>
-    // </div>
-    // <model-viewer
-    //   style={{ height: "100vh", width: "100vw" }}
-    //   src="/assets/models/male_model.glb"
-    //   ar
-    //   ar-scale="fixed"
-    //   camera-controls
-    //   shadow-intensity="1"
-    //   autoplay
-    //   exposure="1"
-    //   xr-environment
-    // >
-    //   <Group
-    //     slot="poster"
-    //     position="center"
-    //     sx={{ width: "100%", height: "100%" }}
-    //   >
-    //     <Loader />
-    //   </Group>
-    //   {/* <Stack
-    //     slot="ar-button"
-    //     justify="flex-end"
-    //     sx={{ width: "100%", height: "100%" }}
-    //   >
-    //     <Button>View in AR</Button>
-    //   </Stack> */}
-    // </model-viewer>
-    <model-viewer
-      src="/assets/models/male_model_final.glb"
-      ar
-      ar-scale="fixed"
-      camera-controls
-      touch-action="pan-y"
-      alt="A 3D model of an astronaut"
-      xr-environment
-    ></model-viewer>
+    <Container>
+      <div>
+        <ARButton />
+        <Canvas dpr={[1, 2]} shadows camera={{ position: [0, 2, 0] }}>
+          <XR referenceSpace="local-floor">
+            {select ? <Character /> : <Marker />}
+            <ambientLight intensity={0.2} />
+            <Character />
+            <Environment preset="city" />
+          </XR>
+        </Canvas>
+      </div>
+      <Box sx={{ width: "100%", height: 500 }}>
+        <model-viewer
+          src="/assets/models/male_model_final.glb"
+          ar
+          camera-controls
+          touch-action="pan-y"
+          alt="A 3D model of an astronaut"
+          xr-environment
+          width={500}
+        ></model-viewer>
+      </Box>
+    </Container>
   )
 }
 
